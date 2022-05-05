5a9d2c5788ba1c9588d10a779bd59545ad2b5a65
leighDEV <leighest2021@gmail.com>
2022-05-05 21:49:34 +0800

    Add statistics.py file

---
+++ statistics.py
@@ -0,0 +1,11 @@
+import re
+import os.path
+
+def read():
+    age_path = "C:\OneDrive\Documents\.School\Code\PycharmProjects\ccs-survey-console\extfiles\\age.txt"
+    if os.path.exists(age_path):
+        f_age = open(age_path, "r")
+        eighteen = re.findall("age", age_path)
+        print(eighteen)
+
+read()
